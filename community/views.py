8eb7164cabba9c3eec86bb7849a8b94b9a7d9abc
Josh Gillette <joshtg.007@gmail.com>
2020-10-03 14:52:57 -0400

    Migrate project to Django

---
+++ community/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+
+def community(request):
+    return render(request, 'community.html')
---
+++ solebuy/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from django.contrib import admin
+
+
+urlpatterns = [
+    path('', include('buyer.urls')),
+    path('community/', include('community.urls')),
+    path('admin/', admin.site.urls),
+]
