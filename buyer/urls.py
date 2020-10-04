98a922c626d320ade821db7cdf4e4bb7d3de1b5c
Josh Gillette <joshtg.007@gmail.com>
2020-10-04 15:15:57 -0400

    Implement forms to send search text to product page

--- buyer/urls.py
+++ buyer/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
 from . import views
 
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('product/', views.product, name='product'),
     path('about/', views.about, name='about'),
 ]
--- buyer/views.py
+++ buyer/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from .forms import SearchForm
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def product(request):
+    form = SearchForm(request.POST)
+    if not form.is_valid():
+        return render(request, 'product.html')
+
+    content = {'text': form.cleaned_data['text']}
+
+    return render(request, 'product.html', content)
 
 
 def about(request):
