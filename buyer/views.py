31cccbe8bb8a349cf8dfb06825f695628dc44e73
Josh Gillette <joshtg.007@gmail.com>
2020-10-08 22:31:29 -0400

    Dynamically create filter inputs from data

--- buyer/views.py
+++ buyer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import SearchForm
+import json
 
 
 def index(request):
@@ -7,11 +8,24 @@ def index(request):
 
 
 def product(request):
+    # Get the search
     form = SearchForm(request.GET)
     if not form.is_valid():
         return render(request, 'product.html')
+    search = form.cleaned_data['search']
 
-    content = {'search': form.cleaned_data['search']}
+    # Lookup the cateogry
+    categoriesData = {}
+    with open('/home/joshtgill/Documents/proj/solebuy-web/local/categories.json', 'r') as filee:
+        categoriesData = json.load(filee)
+    category = {}
+    for categoryData in categoriesData:
+        if search == categoryData.get('name'):
+            category = categoryData
+    if not category:
+        return render(request, 'product.html', {'filterWidth': '100%'})
+
+    content = {'category': category, 'filterWidth': str(len(category.get('assisters')))}
 
     return render(request, 'product.html', content)
 
