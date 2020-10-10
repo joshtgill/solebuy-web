0edada43fecce4eb80dda08dc2d1c62dc71fb919
Josh Gillette <joshtg.007@gmail.com>
2020-10-10 16:49:31 -0400

    Extract input from select form(s)

--- buyer/forms.py
+++ buyer/forms.py
@@ -2,6 +2,15 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-
     attributes = {'placeholder': 'Explore a category', 'autocomplete': 'off'}
     search = forms.CharField(label='', max_length=50, widget=forms.TextInput(attrs=attributes))
+
+
+class SelectForm(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        self.title = kwargs.pop('title')
+        self.choices = kwargs.pop('choices')
+        super(SelectForm, self).__init__(*args, **kwargs)
+
+        self.fields[self.title] = forms.ChoiceField(label='', choices=self.choices)
--- buyer/views.py
+++ buyer/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import SearchForm
+from .forms import SearchForm, SelectForm
 import json
 
 
@@ -25,10 +25,26 @@ def product(request):
     if not category:
         return render(request, 'product.html', {'filterWidth': '100%'})
 
-    # Initialize form
+    # Initialize search form
     searchForm = SearchForm(initial={'search': category.get('name')})
-    content = {'searchForm': searchForm, 'category': category, 'filterWidth': str(len(category.get('assisters')))}
 
+    # Initialize select form(s)
+    selectForms = []
+    for assister in category.get('assisters'):
+        choices = []
+        for filterr in assister.get('filters'):
+            choices.append((filterr, filterr))
+
+        selectForms.append(SelectForm(title=assister.get('label'), choices=tuple(choices)))
+
+    # If request is POST, get value from select form(s)
+    if request.method == 'POST':
+        for form in selectForms:
+            postForm = SelectForm(request.POST, title=form.title, choices=form.choices)
+            if postForm.is_valid():
+                print(postForm.cleaned_data.get(form.title))
+
+    content = {'searchForm': searchForm, 'category': category, 'selectForms': selectForms, 'filterWidth': str(len(selectForms))}
     return render(request, 'product.html', content)
 
 
