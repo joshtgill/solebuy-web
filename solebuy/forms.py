f3331abe05322f45ec70a5ceafa46aa5ad4995a1
Josh Gillette <joshtg.007@gmail.com>
2020-10-14 10:33:07 -0400

    Inspect and clean up home page

--- solebuy/forms.py
+++ solebuy/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class SearchForm(forms.Form):
     attributes = {'placeholder': 'Explore a category', 'autocomplete': 'off'}
-    text = forms.CharField(label='', max_length=50, widget=forms.TextInput(attrs=attributes))
+    text = forms.CharField(label='', max_length=100, widget=forms.TextInput(attrs=attributes))
