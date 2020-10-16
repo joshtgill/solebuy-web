ac2f1aaf7a51d83fdbb8ac7a0bf5e39a5ad1b869
Josh Gillette <joshtg.007@gmail.com>
2020-10-16 14:38:34 -0400

    Inspect and clean up category page

--- buyer/templatetags/helper.py
+++ buyer/templatetags/helper.py
@@ -4,25 +4,15 @@ from django import template
 register = template.Library()
 
 
-@register.filter(name='buildFiltersStyle')
-def buildFiltersStyle(assistersLength):
-    return 'width: {}%'.format((assistersLength * 10) + 30)
-
-
-@register.filter(name='buildFilterStyle')
-def buildFilterStyle(assistersLength):
-    return 'float: left; width: calc(100% / {})'.format(assistersLength)
-
-
 @register.filter(name='buildButtonStyle')
 def buildButtonStyle(buttonValue, idMap):
     assisterId = int(buttonValue.split('.')[0])
     filterId = int(buttonValue.split('.')[1])
 
     if filterId in idMap[assisterId]:
-        return 'background-color: #2385e8; color: #ffffff'
+        return 'border: 3px solid rgb(13, 147, 255);'
     else:
-        return 'background-color: #ffffff; color: #222222; border: 1px solid #222222;'
+        return 'border: 1px solid rgb(170, 170, 170);'
 
 
 @register.filter(name='determineButtonValue')
