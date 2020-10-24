b63546b7dbf33faac8bf6b7f5eece2fd30992a00
Josh Gillette <joshtg.007@gmail.com>
2020-10-24 14:52:35 -0400

    Fill popup with product name, image, and price

--- buyer/static/scripts/category.js
+++ buyer/static/scripts/category.js
@@ -1,16 +1,17 @@
-var productPopup = document.getElementById("productPopup");
+var productPopup = document.getElementById('productPopup');
 
 function openProductPopup(product) {
-    productName = product['name'];
-    console.log(productName);
-	productPopup.style.display = "block";
+    document.getElementById('name').innerText = product['name'];
+    document.getElementById('price').innerText = `$${product['price']}`;
+    document.getElementById('image').src = `/static/images/products/${product['imagePath']}`;
+    productPopup.style.display = "block";
 }
 
 function closeProductPopup() {
-	productPopup.style.display = "none";
+    productPopup.style.display = 'none';
 }
 
-document.getElementsByClassName("close")[0].onclick = function() {
+document.getElementsByClassName('close')[0].onclick = function() {
     closeProductPopup();
 }
 
