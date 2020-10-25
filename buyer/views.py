5ec384ea88d628a5f9f6b9c245db2b595c3f139c
Josh Gillette <joshtg.007@gmail.com>
2020-10-24 20:34:49 -0400

    Refactor idMap to filterIdMap

--- buyer/views.py
+++ buyer/views.py
@@ -47,13 +47,13 @@ def category(request):
         resetIdMap(request, len(categoryData.get('assisters')))
 
     # Find recommended products based on filter selections
-    products = Assistant().findProducts(categoryData, request.session['idMap']).get('primary')
+    products = Assistant().findProducts(categoryData, request.session['filterIdMap']).get('primary')
     products = sorted(products, key=lambda product: product.get('price'))
 
     # Build content for template
     content = {'categoryForm': categoryForm, 'data': categoryData,
                'assisters': categoryData.get('assisters'),
-               'idMap': request.session['idMap'],
+               'filterIdMap': request.session['filterIdMap'],
                'products': products,
                'product': product}
 
@@ -81,17 +81,17 @@ def updateIdMap(request, cleaned_data):
     selectValue = cleaned_data.get('button')
     assisterId = int(selectValue[ : selectValue.find('.')])
     filterId = int(selectValue[selectValue.find('.') + 1 : ])
-    idMap = request.session['idMap']
-    if filterId not in idMap[assisterId]:
-        idMap[assisterId].append(filterId)
+    filterIdMap = request.session['filterIdMap']
+    if filterId not in filterIdMap[assisterId]:
+        filterIdMap[assisterId].append(filterId)
     else:
-        idMap[assisterId].remove(filterId)
+        filterIdMap[assisterId].remove(filterId)
 
-    request.session['idMap'] = idMap
+    request.session['filterIdMap'] = filterIdMap
 
 
 def resetIdMap(request, numAssisters):
-    request.session['idMap'] = [[] for i in range(numAssisters)]
+    request.session['filterIdMap'] = [[] for i in range(numAssisters)]
 
 
 def getProductDataByUrlName(products, urlName):
