78279d7629de904adcc627408fc194b5dd281937
Josh Gillette <joshtg.007@gmail.com>
2020-10-31 18:42:16 -0400

    Redefine STATIC_ROOT

--- solebuy/settings.py
+++ solebuy/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "~/solebuy/static/"
+STATIC_ROOT = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'solebuy/static/',
