c5b8eddc06cc5d47bffab487884f9637d529aaec
Josh Gillette <joshtg.007@gmail.com>
2020-11-10 22:43:46 -0500

    Redesign and enhance product popup

--- buyer/static/scripts/category.js
+++ buyer/static/scripts/category.js
@@ -4,47 +4,44 @@ var productPopup = document.getElementById('productPopup');
 function displayProductPopup(productId) {
     product = JSON.parse(document.getElementById(productId).textContent);
 
+    // Initialize and clear details for fresh popup
+    var detailsDiv = document.getElementById('details');
+    detailsDiv.querySelectorAll('*').forEach(cn => cn.remove());
+
     // Load and display general product data
     document.getElementById('name').innerText = product['name'];
-    document.getElementById('price').innerText = `Starting at $${product['price'].toFixed(2)}`;
     document.getElementById('image').src = `/static/images/smartphone/products/${product['imageFileName']}`;
     document.getElementById('prosSummary').innerText = product['prosSummary'];
     document.getElementById('consSummary').innerText = product['consSummary'];
-
-    // Clear alternatives for fresh load
-    var alternativesDiv = document.getElementById('alternatives');
-    alternativesDiv.querySelectorAll('*').forEach(e => e.remove());
-
-    // Load and display product's alternatives
-    var alternativeProducts = product['alternativeProducts'];
-    if (alternativeProducts.length > 0) {
-        // Create label for alternatives and add to parent
-        var alternativesLabel = document.createElement('h1');
-        alternativesLabel.innerHTML = 'Similar products: ';
-        alternativesDiv.appendChild(alternativesLabel);
-
-        // Create alternative products div
-        var alternativeProductsDiv = document.createElement('div');
-        alternativeProductsDiv.className = 'alternative-products';
-        alternativesDiv.appendChild(alternativeProductsDiv);
-
-        // For each alternative product, create a div containing alternative product name
-        for (i = 0; i < alternativeProducts.length; i++) {
-            var alternativeProductDiv = document.createElement('div');
-            alternativeProductDiv.className = 'alternative-product';
-
-            var alternativeProductH1 = document.createElement('h1');
-            alternativeProductH1.innerHTML = alternativeProducts[i];
-            alternativeProductDiv.appendChild(alternativeProductH1);
-
-            alternativeProductsDiv.appendChild(alternativeProductDiv);
-        }
-    }
+    addDetail(detailsDiv, 'Starting price', `$${product['price'].toFixed(2)}`);
+    addDetail(detailsDiv, 'Starting storage', '128GB');
+    if (product['alternativeProducts'].length > 0)
+        addDetail(detailsDiv, 'Similar products', product['alternativeProducts'].join(', '));
 
     // Show product popup
     productPopup.style.display = "block";
 }
 
+function addDetail(detailsDiv, label, value) {
+    var detailDiv = document.createElement('div');
+    detailDiv.className = 'detail';
+
+    var labelDiv = document.createElement('div');
+    labelDiv.className = 'label';
+    var labelH1 = document.createElement('h1');
+    labelH1.innerHTML = label;
+    labelDiv.appendChild(labelH1);
+    detailDiv.appendChild(labelDiv);
+
+    var valueDiv = document.createElement('div');
+    valueDiv.className = 'value';
+    var valueH1 = document.createElement('h1');
+    valueH1.innerHTML = value;
+    valueDiv.appendChild(valueH1);
+    detailDiv.appendChild(valueDiv);
+
+    detailsDiv.appendChild(detailDiv);
+}
 
 function hideProductPopup() {
     productPopup.style.display = 'none';
