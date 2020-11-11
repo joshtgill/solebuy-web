070fb51dce360d8c556851080d63d3ddd4891a3a
Josh Gillette <joshtg.007@gmail.com>
2020-11-10 19:27:14 -0500

    Refactor all popup alternatives logic to javascript

--- buyer/static/scripts/category.js
+++ buyer/static/scripts/category.js
@@ -4,28 +4,41 @@ var productPopup = document.getElementById('productPopup');
 function displayProductPopup(productId) {
     product = JSON.parse(document.getElementById(productId).textContent);
 
-    // Load general product data
+    // Load and display general product data
     document.getElementById('name').innerText = product['name'];
-    document.getElementById('price').innerText = `$${product['price'].toFixed(2)}`;
+    document.getElementById('price').innerText = `Starting at $${product['price'].toFixed(2)}`;
     document.getElementById('image').src = `/static/images/smartphone/products/${product['imageFileName']}`;
     document.getElementById('prosSummary').innerText = product['prosSummary'];
     document.getElementById('consSummary').innerText = product['consSummary'];
 
-    // Get alternative products div and remove any existing children
-    alternativeProductsDiv = document.getElementById('alternative-products');
-    alternativeProductsDiv.querySelectorAll('*').forEach(n => n.remove());
+    // Clear alternatives for fresh load
+    var alternativesDiv = document.getElementById('alternatives');
+    alternativesDiv.querySelectorAll('*').forEach(e => e.remove());
 
-    // For each alternative product, create a div containing alternative product name
-    alternativeProducts = product['alternativeProducts'];
-    for (i = 0; i < alternativeProducts.length; i++) {
-        var alternativeProductDiv = document.createElement('div');
-        alternativeProductDiv.className = 'alternative-product';
+    // Load and display product's alternatives
+    var alternativeProducts = product['alternativeProducts'];
+    if (alternativeProducts.length > 0) {
+        // Create label for alternatives and add to parent
+        var alternativesLabel = document.createElement('h1');
+        alternativesLabel.innerHTML = 'Similar products: ';
+        alternativesDiv.appendChild(alternativesLabel);
 
-        var alternativeProductH1 = document.createElement('h1');
-        alternativeProductH1.innerHTML = alternativeProducts[i];
+        // Create alternative products div
+        var alternativeProductsDiv = document.createElement('div');
+        alternativeProductsDiv.className = 'alternative-products';
+        alternativesDiv.appendChild(alternativeProductsDiv);
 
-        alternativeProductDiv.appendChild(alternativeProductH1);
-        alternativeProductsDiv.appendChild(alternativeProductDiv);
+        // For each alternative product, create a div containing alternative product name
+        for (i = 0; i < alternativeProducts.length; i++) {
+            var alternativeProductDiv = document.createElement('div');
+            alternativeProductDiv.className = 'alternative-product';
+
+            var alternativeProductH1 = document.createElement('h1');
+            alternativeProductH1.innerHTML = alternativeProducts[i];
+            alternativeProductDiv.appendChild(alternativeProductH1);
+
+            alternativeProductsDiv.appendChild(alternativeProductDiv);
+        }
     }
 
     // Show product popup
